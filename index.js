e967ade504238615eb2e48f597a6418d3419e75e
skjwise <skjwise@hotmail.com>
2019-11-04 11:13:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, search){
+    return array.filter(element => element.toLowerCase() === search.toLowerCase());
+}
+
+function fuzzyMatch(array, search){
+    return array.filter(possibleMatch => possibleMatch.toLowerCase().indexOf(search.toLowerCase())=== 0);
+}
+
+function matchName(array, search){
+    return array.filter(record => record.name === search);
+}
